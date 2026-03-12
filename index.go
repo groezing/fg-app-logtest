9a9492cfc8907848c80329fb119735ac09c7019f
groezing_pub <80272930+groezing@users.noreply.github.com>
2026-03-12 16:06:56 +0100

    Update index.go

--- index.go
+++ index.go
@@ -1,1 +1,20 @@
-a
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var num_called int = 0 
+
+func greet(w http.ResponseWriter, r *http.Request) {
+  num_called+=1 
+  fmt.Fprintf(w, "fg Hello World called %d ! %s", num_called,  time.Now())
+  os.exit(1)
+}
+
+func main() {
+	http.HandleFunc("/", greet)
+	http.ListenAndServe(":8080", nil)
+}
