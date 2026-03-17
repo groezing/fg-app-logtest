291f2c751a8ab87f6fdd40e015fae83ccc72db86
groezing_pub <80272930+groezing@users.noreply.github.com>
2026-03-17 13:58:50 +0100

    Update main.go

--- main.go
+++ main.go
@@ -12,9 +12,9 @@ var num_called int = 0
 func greet(w http.ResponseWriter, r *http.Request) {
   num_called+=1 
 
-  fmt.Println( "{ctx: kafka-client, Adding consumer}");
-  fmt.Println( "{ctx: kafka-client, Consumer added >> {}}");
-  fmt.Println( "{ctx: app,\n####                                 ######                                                  \n #     # #       ####   ####  ######    #     # #####   ####  ##### #    # ###### #####   ####  }");
+  fmt.Println( "2026-03-17T12:28:53.469Z info {ctx: kafka-client, Adding consumer}");
+  fmt.Println( "2026-03-17T12:28:53.469Z info {ctx: kafka-client, Consumer added >> {}}");
+  fmt.Println( "2026-03-17T12:28:53.469Z info {ctx: app,\n####                                 ######                                                  \n #     # #       ####   ####  ######    #     # #####   ####  ##### #    # ###### #####   ####  }");
   
   // fmt.Printf( "fg Hello World called %d ! %s\n", num_called,  time.Now() )
   fmt.Fprintf(w, "fg Hello World called %d ! %s", num_called,  time.Now())
