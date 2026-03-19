88bcd8e5b877c294aa608f5c7f1814479c72f13f
groezing_pub <80272930+groezing@users.noreply.github.com>
2026-03-19 16:26:38 +0100

    Update main.go

--- main.go
+++ main.go
@@ -12,11 +12,11 @@ var num_called int = 0
 func greet(w http.ResponseWriter, r *http.Request) {
   num_called+=1 
 
-  fmt.Println( "2026-03-17T12:28:53.469Z info {ctx: kafka-client, Adding consumer}");
-  fmt.Println( "2026-03-17T22:28:53.469Z info {ctx: kafka-client, Consumer added >> {}}");
-  fmt.Println( "2026-03-17T12:28:53.469Z info {ctx: app,\n####                                 ######                                                  \n #     # #       ####   ####  ######    #     # #####   ####  ##### #    # ###### #####   ####  }");
+ // fmt.Println( "2026-03-17T12:28:53.469Z info {ctx: kafka-client, Adding consumer}");
+ // fmt.Println( "2026-03-17T22:28:53.469Z info {ctx: kafka-client, Consumer added >> {}}");
+ // fmt.Println( "2026-03-17T12:28:53.469Z info {ctx: app,\n####                                 ######                                                  \n #     # #       ####   ####  ######    #     # #####   ####  ##### #    # ###### #####   ####  }");
   
-  // fmt.Printf( "fg Hello World called %d ! %s\n", num_called,  time.Now() )
+  fmt.Println( "fgHelloWorld App on stage " )
   fmt.Fprintf(w, "fg Hello World called %d ! %s", num_called,  time.Now())
   //os.Exit(1)
 }
